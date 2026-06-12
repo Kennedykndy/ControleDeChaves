39fcc23910090413c8cec3244f26ba9cbb599332
Kennedykndy <dev.senac.kennedy@gmail.com>
2026-06-12 20:17:18 -0300

    Alterando package.json

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: "/ControleDeChaves/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
